f914ee403aba021804681e3527c162d21f2d6d8e
averylytle <85377495+averylytle@users.noreply.github.com>
2022-07-22 10:46:31 -0600

    Update streamlit_app.py
    adding breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('some item')
+streamlit.text('some item')
+streamlit.text('some item')
